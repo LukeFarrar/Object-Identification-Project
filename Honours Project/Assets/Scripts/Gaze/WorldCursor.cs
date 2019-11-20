dbb3b6669b70480934b18a652b88c20f3d2e9afa
Berrale <lukebfarrar@gmail.com>
2019-11-20 23:54:47 +0000

    Gaze bug fixed

--- Honours Project/Assets/Scripts/Gaze/WorldCursor.cs
+++ Honours Project/Assets/Scripts/Gaze/WorldCursor.cs
@@ -4,38 +4,43 @@ public class WorldCursor : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        //Grab the mesh renderer
+        // Grab the mesh renderer that's on the same object as this script.
         meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Raycast into the world based on the user's head position and orientation.
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
         var headPosition = Camera.main.transform.position;
         var gazeDirection = Camera.main.transform.forward;
 
         RaycastHit hitInfo;
 
-        if(Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
         {
-            //If the raycast hits a hologram, display the mesh
-            meshRenderer.enabled = true;
+            if(gameObject != this)
+            {
+                // If the raycast hit a hologram...
+                // Display the cursor mesh.
+                meshRenderer.enabled = true;
 
-            //Move the cursor to the point where the raycast hits.
-            this.transform.position = hitInfo.point;
+                // Move thecursor to the point where the raycast hit.
+                this.transform.position = hitInfo.point;
 
-            //Rotate the cursor to hug the surface of the hologram.
-            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                // Rotate the cursor to hug the surface of the hologram.
+                this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            }
+            
         }
         else
         {
-            //If the raycast did not hit a hologram, hide the cursor mesh.
-            meshRenderer.enabled = true;
+            // If the raycast did not hit a hologram, hide the cursor mesh.
+            meshRenderer.enabled = false;
         }
     }
 }
