47ec5614e87e88760d4147838f71e84a01c2fe73
Berrale <lukebfarrar@gmail.com>
2019-11-25 15:01:17 +0000

    Fixed some bugs, added spatial sound to UI as well as spatial mapping for it. Changed the look of the cursor. May need additional tweaks

---
+++ Honours Project/Assets/Scripts/Gaze/Billboarding.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public enum Axis
+{
+    X, Y, Z, XY, XZ, YZ, None
+}
+public class Billboarding : MonoBehaviour
+{
+    public Axis axis = Axis.None;
+    public Transform target;
+
+    private void OnEnable()
+    {
+        if(target == null)
+        {
+            target = Camera.main.transform;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (target == null)
+            return;
+
+        Vector3 upDir = new Vector3(0, 1, 0);
+        Vector3 targetDir = target.position - transform.position;
+        Vector3 targetUpVector = Camera.main.transform.up;
+
+        switch(axis)
+        {
+            case Axis.X:
+                targetDir.x = 0.0f;
+                targetUpVector = upDir;
+                break;
+            case Axis.Y:
+                targetDir.y = 0.0f;
+                targetUpVector = upDir;
+                break;
+            case Axis.Z:
+                targetDir.x = 0.0f;
+                targetDir.y = 0.0f;
+                break;
+            case Axis.XY:
+                targetUpVector = upDir;
+                break;
+            case Axis.XZ:
+                targetDir.x = 0.0f;
+                break;
+            case Axis.YZ:
+                targetDir.y = 0.0f;
+                break;
+        }
+
+        transform.rotation = Quaternion.LookRotation(-targetDir, targetUpVector);
+    }
+}
+*/
---
+++ Honours Project/Assets/Scripts/Gaze/FollowAlong.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAlong : MonoBehaviour
+{
+    bool placing = false;
+
+    /* There was a bug where the UI would collide with it's self causing it to move
+     * towards the camera. I am using the following to disable the UI from 
+     * colliding with itself.
+     */
+    public bool applyToBaseObject = false;
+    void Awake()
+    {
+        if (applyToBaseObject == true)
+        {
+            Collider collider = GetComponent<Collider>();
+            if (collider != null)
+            {
+                foreach (Transform child in transform)
+                {
+                    Collider secondCollider = child.GetComponent<Collider>();
+                    if (secondCollider == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Physics.IgnoreCollision(collider, secondCollider);
+                    }
+                }
+            }
+        }
+
+        foreach(Transform child in transform)
+        {
+            Collider collider = child.GetComponent<Collider>();
+            if (collider == null)
+                continue;
+            else
+            {
+                foreach(Transform secondChild in transform)
+                {
+                    Collider secondCollider = secondChild.GetComponent<Collider>();
+                    if(secondCollider == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Physics.IgnoreCollision(collider, secondCollider);
+                    }
+                }
+            }
+        }
+    }
+
+    void OnSelect()
+    {
+        // On each Select gesture, toggle whether the user is in placing mode.
+        placing = !placing;
+
+        // If the user is in placing mode, display the spatial mapping mesh.
+        if (placing)
+        {
+            SpatialMapping.Instance.DrawVisualMeshes = true;
+        }
+        // If the user is not in placing mode, hide the spatial mapping mesh.
+        else
+        {
+            SpatialMapping.Instance.DrawVisualMeshes = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (placing)
+        {
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            Ray ray = new Ray(headPosition, gazeDirection);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+            {
+                RaycastHit[] Allhits = Physics.RaycastAll(ray, 100f);
+                foreach (RaycastHit hit in Allhits)
+                {
+                    if (!hit.transform.IsChildOf(this.transform))
+                    {
+                        if (gameObject != this)
+                        {
+                            this.transform.position = new Vector3(
+                            hitInfo.point.x,
+                            hitInfo.point.y,
+                            hitInfo.point.z
+                            );
+
+                            this.transform.position = this.transform.position + gazeDirection * 0.001f;
+
+                            Quaternion toQuat = Camera.main.transform.localRotation;
+                            this.transform.rotation = toQuat;
+
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+    }
+}
