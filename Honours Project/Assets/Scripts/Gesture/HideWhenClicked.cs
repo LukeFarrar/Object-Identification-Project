4e779fd0c55a2a9f37648a7f0e428728de3dff27
Berrale <lukebfarrar@gmail.com>
2019-11-29 19:03:13 +0000

    Added billboading and tagalong to the dynamic UI holder and enabled voice input to show and hide it. Need to have it disable the collision

---
+++ Honours Project/Assets/Scripts/Gesture/HideWhenClicked.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWhenClicked : MonoBehaviour
+{
+    MeshRenderer thisMesh = new MeshRenderer();
+    // Start is called before the first frame update
+    void OnSelect()
+    {
+        thisMesh = gameObject.GetComponent<MeshRenderer>();
+        thisMesh.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Honours Project/Assets/Scripts/Voice/SpeechShow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechShow : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    private MeshRenderer thisMesh;
+    private Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialize
+        thisMesh = this.gameObject.GetComponentInParent<MeshRenderer>();
+        canvas = this.gameObject.GetComponent<Canvas>();
+
+        keywords.Add("Hide", () =>
+        {
+            HideDisplayCalled();
+        });
+
+        keywords.Add("Show", () =>
+        {
+            ShowDisplayCalled();
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizerOnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    private void HideDisplayCalled()
+    {
+        thisMesh.enabled = false;
+        canvas.enabled = false;
+    }
+
+    private void ShowDisplayCalled()
+    {
+        thisMesh.enabled = true;
+        canvas.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
